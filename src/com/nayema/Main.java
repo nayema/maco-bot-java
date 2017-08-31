79d4266d17bd0e6c1b7d1ca20f671b11c8fa07dc
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-31 15:25:59 -0400

    Fixes minor formatting in Main.

--- src/com/nayema/Main.java
+++ src/com/nayema/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         ADICalculation adiValue = new ADICalculation("", "2.75", "40.0");
         L4Calculation l4Calculator = new L4Calculation(adiValue);
+
         l4Calculator.setTotalSharedSurfaceArea(1002.5);
-        l4Calculator.calculateL4();
 
         System.out.println("ADI is: " + adiValue.adi);
+        l4Calculator.calculateL4();
         System.out.println("L4 is: " + l4Calculator.l4);
     }
 }
