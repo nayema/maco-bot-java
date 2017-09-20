8b754e91c2839ac6dd572ca6f1dc3bf262762663
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-20 17:12:54 -0400

    Removes src/ folder folder from .gitignore

---
+++ src/com/nayema/ADICalculator.java
@@ -0,0 +1,20 @@
+package com.nayema;
+
+public class ADICalculator {
+    private double minOfADEOrPDE;
+    private double ttc;
+    private double mdd;
+
+    public ADICalculator(double minOfADEOrPDE, double ttc, double mdd) {
+        this.minOfADEOrPDE = minOfADEOrPDE;
+        this.ttc = ttc;
+        this.mdd = mdd;
+    }
+
+    public double calculateADI() {
+        if (minOfADEOrPDE == 0) {
+            return Math.min(ttc, mdd);
+        }
+        return minOfADEOrPDE;
+    }
+}
---
+++ src/com/nayema/L4Calculator.java
@@ -0,0 +1,31 @@
+package com.nayema;
+
+public class L4Calculator {
+    private double adi;
+    private double batchSizeOfNextProductInKG;
+    private double totalSharedSurfaceArea;
+    
+    public L4Calculator(double adi, double batchSizeOfNextProductInKG, double totalSharedSurfaceArea) {
+        this.adi = adi;
+        this.batchSizeOfNextProductInKG = batchSizeOfNextProductInKG;
+        this.totalSharedSurfaceArea = totalSharedSurfaceArea;
+    }
+
+    public double calculateL4() {
+        return calculateL3() * 100;
+    }
+
+    public double calculateL3() {
+        return calculateL2() / totalSharedSurfaceArea;
+    }
+
+    public double calculateL2() {
+        double CONVERSION_RATIO_TO_MICROGRAMS = 1000000000.00;
+        return calculateL1() * (batchSizeOfNextProductInKG * CONVERSION_RATIO_TO_MICROGRAMS);
+    }
+
+    public double calculateL1() {
+        double MAX_DOSE_OF_NEXT_PRODUCT = 2000.00;
+        return adi / MAX_DOSE_OF_NEXT_PRODUCT;
+    }
+}
